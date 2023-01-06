648fa282579f7d94d09f1df3787bea6cb0e663d1
PraveenKusuluri08 <praveen.k@skil.ai>
2023-01-06 21:38:10 +0530

    Multiple file upload for the 'ecommerse-products-service'

--- Controller/fileUpload.js
+++ Controller/fileUpload.js
@@ -1,38 +1,27 @@
-const Busboy = require("busboy");
-const { v4: uuidv4 } = require("uuid");
-const AWS = require("aws-sdk");
-const multer  = require('multer');
-const multerS3 = require('multer-s3');
-const s3 = new AWS.S3();
+const Busboy = require("busboy")
+const { v4: uuidv4 } = require("uuid")
+const AWS = require("aws-sdk")
+const s3 = new AWS.S3()
 
 const imageUploadExtractLink = (req, res) => {
-  let busboy = Busboy({ headers: req.headers });
-  const fID = uuidv4();
-  let chunkImage = [];
-  let fName, fEncoding, fType;
+  let busboy = Busboy({ headers: req.headers })
+  const fID = uuidv4()
+  let chunkImage = []
+  let fName, fEncoding, fType
   busboy.on("file", (fieldname, file, info) => {
-    const { filename, encoding, mimeType } = info;
-    if (
-      mimeType !== "image/jpeg" &&
-      mimeType !== "image/png" &&
-      mimeType !== "jpg"
-    ) {
-      return res.status(404).json({
-        message: "Please give only 'png/jpeg/jpg'",
-      });
-    }
-    const imageExtension = filename.split(".")[filename.split(".").length - 1];
-    fName = `ImageUploadS3${fID}.${imageExtension}`;
-    fEncoding = encoding;
-    fType = mimeType;
+    const { filename, encoding, mimeType } = info
+    const imageExtension = filename.split(".")[filename.split(".").length - 1]
+    fName = `ImageUploadS3${fID}.${imageExtension}`
+    fEncoding = encoding
+    fType = mimeType
     file.on("data", (data) => {
-      console.log(data);
-      chunkImage.push(data);
-    });
+      console.log(data)
+      chunkImage.push(data)
+    })
     file.on("end", () => {
-      console.log("File" + filename + "Processed");
-    });
-  });
+      console.log("File" + filename + "Processed")
+    })
+  })
   busboy.on("finish", () => {
     const Params = {
       Bucket: "nodejs-image-upload",
@@ -42,20 +31,57 @@ const imageUploadExtractLink = (req, res) => {
       ACL: "public-read",
       ContentEncoding: fEncoding,
       ContentType: fType,
-    };
+    }
     s3.upload(Params, (err, info) => {
-      console.log("Info", info);
+      console.log("Info", info)
       if (err) {
-        return res.status(400).json({ err, error: true });
+        return res.status(400).json({ err, error: true })
       } else {
         return res.status(201).send({
           ImageInfo: info.Location,
           message: "Image Uploaded Successfully",
-        });
+        })
       }
-    });
-  });
-  req.pipe(busboy);
-};
+    })
+  })
+  req.pipe(busboy)
+}
 
-module.exports = { imageUploadExtractLink,imageUploadS3 };
+const multipleImageUpload = (req, res, err) => {
+  let imageUrls=[]
+  let itemId=req.body["itemId"]
+  let params
+  const files = req.files
+  console.log(files, req.body["itemId"])
+  for(let i=0;i<files.length;i++) {
+    if(files[i].mimetype!=="image/png"&&files[i].mimetype!=="image/jpg"&&images[i].mimetype!=="image/jpeg") {
+      console.log(files[i]["mimetype"]!=="image/png");
+      return res.status(404).json({
+        message:"Please give only 'png/jpeg/jpg'"
+      })
+    }
+  }
+  console.log("first",files.length)
+  for (let i = 0; i < files.length; i++) {
+    params={
+      Bucket:"commerse-products",
+      Key: `${itemId}/${new Date().getTime()}-${files[i]["mimetype"]}-${files[i]["originalname"]}`,
+      Body: files[i]["buffer"],
+      ACL: "public-read",
+    }
+    s3.upload(params,((err,info)=>{
+      console.log("first->",info)
+      if(err) {
+        console.log(err)
+         res.status(400).json({err,error:true})
+      } else{
+        imageUrls.push(info)
+        if(imageUrls.length == files.length){
+        return res.json({ "error": false, "Message": "File Uploaded SuceesFully", Data: imageUrls});
+      }
+    }
+    }))
+  }
+}
+
+module.exports = { imageUploadExtractLink, multipleImageUpload }
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
-const { imageUploadExtractLink} = require("../Controller/fileUpload");
+const { imageUploadExtractLink, multipleImageUpload} = require("../Controller/fileUpload");
+const multer = require("multer");
+
+let upload = multer().array("files", 4);
 
 router.post("/s3/upload", imageUploadExtractLink);
 
+router.post("/multipleimagesupload",upload,multipleImageUpload)
 
 module.exports = router;
