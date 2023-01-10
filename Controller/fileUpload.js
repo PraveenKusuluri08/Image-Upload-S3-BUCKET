fb888667461b4964ceab8e8d77f1d883f2debca3
PraveenKusuluri08 <praveen.k@skil.ai>
2023-01-10 22:02:03 +0530

    Implemented endpoint for commerse image upload for products

--- Controller/fileUpload.js
+++ Controller/fileUpload.js
@@ -48,40 +48,105 @@ const imageUploadExtractLink = (req, res) => {
 }
 
 const multipleImageUpload = (req, res, err) => {
-  let imageUrls=[]
-  let itemId=req.body["itemId"]
+  let imageUrls = []
+  let itemId = req.body["itemId"]
   let params
   const files = req.files
   console.log(files, req.body["itemId"])
-  for(let i=0;i<files.length;i++) {
-    if(files[i].mimetype!=="image/png"&&files[i].mimetype!=="image/jpg"&&images[i].mimetype!=="image/jpeg") {
-      console.log(files[i]["mimetype"]!=="image/png");
+  for (let i = 0; i < files.length; i++) {
+    if (
+      files[i].mimetype !== "image/png" &&
+      files[i].mimetype !== "image/jpg" &&
+      images[i].mimetype !== "image/jpeg"
+    ) {
+      console.log(files[i]["mimetype"] !== "image/png")
       return res.status(404).json({
-        message:"Please give only 'png/jpeg/jpg'"
+        message: "Please give only 'png/jpeg/jpg'",
       })
     }
   }
-  console.log("first",files.length)
+  console.log("first", files.length)
   for (let i = 0; i < files.length; i++) {
-    params={
-      Bucket:"commerse-products",
-      Key: `${itemId}/${new Date().getTime()}-${files[i]["mimetype"]}-${files[i]["originalname"]}`,
+    params = {
+      Bucket: "commerse-products",
+      Key: `${itemId}/${new Date().getTime()}-${files[i]["mimetype"]}-${
+        files[i]["originalname"]
+      }`,
       Body: files[i]["buffer"],
       ACL: "public-read",
     }
-    s3.upload(params,((err,info)=>{
-      console.log("first->",info)
-      if(err) {
+    s3.upload(params, (err, info) => {
+      console.log("first->", info)
+      if (err) {
         console.log(err)
-         res.status(400).json({err,error:true})
-      } else{
+        res.status(400).json({ err, error: true })
+      } else {
         imageUrls.push(info)
-        if(imageUrls.length == files.length){
-        return res.json({ "error": false, "Message": "File Uploaded SuceesFully", Data: imageUrls});
+        if (imageUrls.length == files.length) {
+          return res.json({
+            error: false,
+            Message: "File Uploaded SuceesFully",
+            Data: imageUrls,
+          })
+        }
       }
-    }
-    }))
+    })
+  }
+}
+const multipleImageUploadCommerseProducts = async (req, res, err) => {
+  try {
+    return await new Promise((resolve, reject) => {
+      let imageUrls = []
+      let itemId = req.body["data"].at(4)
+      let params, bufferData
+      const files = JSON.parse([...req.body.data].at(1))
+      console.log("files😆", files[0])
+      console.log("first", [...req.body.data].at(4))
+
+      files[0].forEach((file) => {
+        console.log("first->🤣", file)
+        if (file.mimetype !== "image/png" &&
+          file.mimetype !== "image/jpg" &&
+          file.mimetype !== "image/jpeg") {
+          console.log(file["mimetype"] !== "image/png")
+          return reject("Please give only 'png/jpeg/jpg'")
+        }
+      })
+      files[0].forEach((file_1) => {
+        console.log("file:--------------------------------", file_1)
+        bufferData = Buffer.from(file_1["buffer"], "binary")
+        params = {
+          Bucket: "commerse-products",
+          Key: `${itemId}/${new Date().getTime()}-${file_1["mimetype"]}-${file_1["originalname"]}`,
+          Body: bufferData,
+          ACL: "public-read",
+        }
+        s3.upload(params, (err_1, info) => {
+          console.log("first->", info)
+          if (err_1) {
+            console.log(err_1)
+            throw err_1
+          } else {
+            imageUrls.push(info)
+            if (imageUrls.length == files[0].length) {
+              return res.json({
+                error: false,
+                Message: "File Uploaded SuceesFully",
+                Data: imageUrls,
+              })
+            }
+          }
+        })
+      })
+    })
+  } catch (err_2) {
+    console.log(err_2)
+    return err_2
   }
 }
 
-module.exports = { imageUploadExtractLink, multipleImageUpload }
+module.exports = {
+  imageUploadExtractLink,
+  multipleImageUpload,
+  multipleImageUploadCommerseProducts,
+}
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
-const { imageUploadExtractLink, multipleImageUpload} = require("../Controller/fileUpload");
+const { imageUploadExtractLink, multipleImageUpload, multipleImageUploadCommerseProducts} = require("../Controller/fileUpload");
 const multer = require("multer");
 
-let upload = multer().array("files", 4);
+let upload = multer().array("files",4)
+
+let upload1 = multer().any()
 
 router.post("/s3/upload", imageUploadExtractLink);
 
 router.post("/multipleimagesupload",upload,multipleImageUpload)
+
+router.post("/multipleimagesuploadcommerseproducts",upload1,multipleImageUploadCommerseProducts)
 
 module.exports = router;
